e27cf18d9be28de35a560166ccf8b0fe67ba6b8b
Khori-Lee <otherstufflee@gmail.com>
2022-10-15 13:17:46 -0500

    Script update from Sat Oct 15 01:17:46 PM EST 2022

--- Update Icon Pack.sh
+++ Update Icon Pack.sh
@@ -4,13 +4,16 @@ rm -rf ~/DATA/Theme-Icon-Packs/
 
 git clone https://github.com/Khori-Lee/Theme-Icon-Packs.git ~/DATA/Theme-Icon-Packs
 
+
 cp -r ~/.icons/* ~/DATA/Theme-Icon-Packs/.icons/
 cp -r ~/.themes/* ~/DATA/Theme-Icon-Packs/.themes/
 
 echo
 
-~/DATA/Theme-Icon-Packs/setup-git-v1.sh
+cd ~/DATA/Theme-Icon-Packs/
+
+setup-git-v1.sh
 
 echo
 
-~/DATA/Theme-Icon-Packs/git-v3-auto.sh
+git-v3-auto.sh
