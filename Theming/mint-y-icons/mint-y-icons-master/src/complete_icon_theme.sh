ee6e2b03f8708bf3c7a012c6ef2ecb7317d7f39c
Khori-Lee <otherstufflee@gmail.com>
2022-10-15 12:32:31 -0500

    Script update from Sat Oct 15 12:32:31 PM EST 2022

--- Theming/mint-y-icons/mint-y-icons-master/src/complete_icon_theme.sh
+++ Theming/mint-y-icons/mint-y-icons-master/src/complete_icon_theme.sh
@@ -8,9 +8,9 @@ echo "################################################################## "
 echo "                          Renaming Folder                          "
 echo "################################################################## "
 
-mv /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Mint-Y-$color/ /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/
+mv ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Mint-Y-$color/ ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/
 
-cd /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/places
+cd ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/places
 rm -r *2x
 
 echo
@@ -18,9 +18,9 @@ echo "################################################################## "
 echo "                 Copying files and Update Index.theme              "
 echo "################################################################## "
 
-cp -r /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Mint-Y/Special_Icons/* /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color
+cp -r ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Mint-Y/Special_Icons/* ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color
 
-sed -i 's|Name=Surfn Mint Y|Name=Surfn Mint Y '$color'|' /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/index.theme 
+sed -i 's|Name=Surfn Mint Y|Name=Surfn Mint Y '$color'|' ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/index.theme 
 
 echo
 echo "################################################################## "
@@ -28,18 +28,18 @@ echo "                  Creating New Icon Theme Cache                    "
 echo "################################################################## "
 echo
 
-cd /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/
-sh /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/create-new-icon-theme.cache.sh
+cd ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/
+sh ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/create-new-icon-theme.cache.sh
 
 echo
 echo "################################################################## "
 echo "                Moving file to Icon themes Folder                  "
 echo "################################################################## "
 
-cd /home/lee/.icons/
+cd ~/.icons/
 rm -r Surfn-Mint-Y-$color
-cd /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/
-mv /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/ /home/lee/.icons/
+cd ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/
+mv ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/usr/share/icons/Surfn-Mint-Y-$color/ ~/.icons/
 
 echo
 echo "################################################################## "
--- Theming/mint-y-icons/mint-y-icons-master/src/places/create-color.sh
+++ Theming/mint-y-icons/mint-y-icons-master/src/places/create-color.sh
@@ -8,21 +8,21 @@ newcol="COLORS['"$colorname"']"=""'"'$hex'"'""
 
 # = '"66a8cb"'
 
-sed -i '20 a '$newcol'' /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/src/places/generate-color-variations.py
+sed -i '20 a '$newcol'' ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/src/places/generate-color-variations.py
 
 python3 ./generate-color-variations.py 
 
-cd /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/src/
+cd ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/src/
 
 color=${colorname^}
 
-sed -i 's/colors \= \[/colors \= \["'$color'",/ ' /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/src/render_places.py 
-sed -i 's/color can be:/color can be: '$color',/ ' /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/src/render_places.py 
+sed -i 's/colors \= \[/colors \= \["'$color'",/ ' ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/src/render_places.py 
+sed -i 's/color can be:/color can be: '$color',/ ' ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/src/render_places.py 
 
 python3 ./render_places.py $color
 
-cd /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/src/
+cd ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/src/
 
 export colorname
 
-sh /home/lee/Desktop/Theming/mint-y-icons/mint-y-icons-master/src/complete_icon_theme.sh
+sh ~/DATA/New-System/Theming/mint-y-icons/mint-y-icons-master/src/complete_icon_theme.sh
