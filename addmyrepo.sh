9e61b6c9478fd0c7ba9a9fea3c9282f0497aa013
Khori-Lee <otherstufflee@gmail.com>
2022-10-15 19:13:07 -0500

    Script update Sat Oct 15 07:13:07 PM EST 2022

--- addmyrepo.sh
+++ addmyrepo.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #add my repos to pacman.conf and update database
+#change address of the link variable below to your repo address ending .io leave of the actuall repo name
+#eg https://khori-lee.github.io instead of https://khori-lee.github.io/yourreponame
+link="https://khori-lee.github.io"
 
 read -p "Name of repo to Add: " name
 echo
@@ -11,7 +14,7 @@ echo
 echo '
 ['$name']
 SigLevel = Optional TrustedOnly
-Server = https://khori-lee.github.io/$repo/$arch' | sudo tee -a /etc/pacman.conf
+Server = '$link'/$repo/$arch' | sudo tee -a /etc/pacman.conf
 echo
 echo "################################################################"
 echo "###################    Updating Database     ###################"
