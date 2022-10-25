fd73a25daec4710a26d82f3c266c881779e4e283
Khori-Lee <otherstufflee@gmail.com>
2022-10-24 20:45:49 -0500

    Updated Mon Oct 24 08:45:49 PM EST 2022

---
+++ Logout/archlinux-logout-gui-fix.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+
+sudo mv  /usr/share/archlinux-logout/GUI.py /usr/share/archlinux-logout/GUI.py.bak 
+sudo cp  ~/DATA/New-Device/Logout/GUI.py /usr/share/archlinux-logout/
+
+
